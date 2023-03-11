3316dd44a9d516d24e9e87ddf075e1ee1cd9d7ba
hammsvietro <hammsvietro@gmail.com>
2023-03-11 01:46:12 -0300

    Implements recursive file listing

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,30 @@
 module Main where
 
+import Control.Exception
+import Control.Monad
+import Data.List (partition)
+import Data.Typeable
+import System.Console.ANSI
 import System.Directory
 import System.Environment
 import System.Exit
 import System.FilePath
 import System.IO (hPutStrLn, stderr)
+import Text.Regex.Posix
 
-type FileVerification = Either String FilePath
+data FuncFindError
+  = InvalidDirectory
+  | FileNotAllowed
+  | IncorrectArgs
+  deriving (Typeable)
+
+instance Show FuncFindError where
+  show error = case error of
+    InvalidDirectory -> "The directory provided doesn't exist."
+    FileNotAllowed -> "Expected a folder as an input, but got a file."
+    IncorrectArgs -> "usage:\nfuncfind [PATH] [EXPRESSION]"
+
+instance Exception FuncFindError
 
 exitWithErrorMessage :: String -> ExitCode -> IO a
 exitWithErrorMessage str e = hPutStrLn stderr str >> exitWith e
@@ -23,23 +41,36 @@ parseDirectory = do
     Nothing -> return currentDirectory
     Just path -> return (currentDirectory </> path)
 
-checkFileExists :: FilePath -> IO FileVerification
+checkFileExists :: FilePath -> IO FilePath
 checkFileExists path =
   doesDirectoryExist path
     >>= ( \exists ->
             if exists
-              then return (Right path)
-              else return (Left "Direcotry doesn't exist.")
+              then return path
+              else throwIO InvalidDirectory
         )
 
-handleFileVerification :: FileVerification -> IO FilePath
-handleFileVerification (Left err) = exitWithErrorMessage err $ ExitFailure $ -1
-handleFileVerification (Right path) = return path
+partitionM :: (Monad m) => (a -> m Bool) -> [a] -> m ([a], [a])
+partitionM p = foldM f ([], [])
+ where
+  f (a, b) x = do
+    flag <- p x
+    return $
+      if flag
+        then (x : a, b)
+        else (a, x : b)
+
+listDirectories :: FilePath -> IO [FilePath]
+listDirectories entrypoint = do
+  putStrLn entrypoint
+  (files, directories) <- listDirectory entrypoint >>= partitionM doesFileExist
+  foldM (\a c -> listDirectories c >>= \items -> return $ items ++ a) files directories
 
 main :: IO ()
 main =
-  parseDirectory
-    >>= checkFileExists
-    >>= handleFileVerification
-    >>= listDirectory
-    >>= mapM_ putStrLn
+  handle
+    (\InvalidDirectory -> exitWithErrorMessage (show InvalidDirectory) (ExitFailure 1))
+    $ parseDirectory
+      >>= checkFileExists
+      >>= listDirectories
+      >>= mapM_ putStrLn
